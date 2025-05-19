39b0b329ceb21826b127f125cb521d4d4e1e6216
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-19 15:47:50 +0000

    Update main.tf

--- modules/harness-aws-irsa-connectors/main.tf
+++ modules/harness-aws-irsa-connectors/main.tf
@@ -13,7 +13,7 @@ resource "harness_platform_connector_azure_cloud_provider" "inherit_from_delegat
     azure_inherit_from_delegate_details {
       auth {
         azure_msi_auth_ua {
-          client_id = "client_id"
+          client_id = "511828f0-91b6-4287-8b61-763a7b339670"
         }
         type = "UserAssignedManagedIdentity"
       }
