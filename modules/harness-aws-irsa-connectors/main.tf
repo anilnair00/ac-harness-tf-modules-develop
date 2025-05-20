2d0c8e76483cd7f6b03ae8ec97607177caefc7e7
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2025-05-20 14:12:12 +0000

    Update main.tf

--- modules/harness-aws-irsa-connectors/main.tf
+++ modules/harness-aws-irsa-connectors/main.tf
@@ -21,6 +21,9 @@ resource "harness_platform_connector_azure_cloud_provider" "inherit_from_delegat
   }
   azure_environment_type = "AZURE"
 #  delegate_selectors     = ["harness-delegate"]
-   delegate_selectors     = ["$(each.value.harnessdelegate}"]
-   
+   irsa {
+     delegate_selectors     = ["$(each.value.harnessdelegate}"]
+      region                = each.value.region
+   }
+   tags = var.tags 
 }
