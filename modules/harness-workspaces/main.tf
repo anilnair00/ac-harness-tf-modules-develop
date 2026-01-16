9b57f50cf5cc793b39ee2eb0ac585d13f5a26b10
anilnair00 <168839295+anilnair00@users.noreply.github.com>
2026-01-16 06:26:18 +0000

    Refactor workspace resource to use var.workspace

--- modules/harness-workspaces/main.tf
+++ modules/harness-workspaces/main.tf
@@ -1,38 +1,36 @@
 resource "harness_platform_workspace" "workspace" {
-  for_each = { for idx, workspace in var.workspaces : idx => workspace }
+#  for_each = { for idx, workspace in var.workspaces : idx => workspace }
+  identifier              = var.workspace.identifier
+  name                    = var.workspace.name
+  org_id                  = var.workspace.org_id
+  project_id              = var.workspace.project_id
+  cost_estimation_enabled = var.workspace.cost_estimation_enabled
+  provisioner_type        = var.workspace.provisioner_type
+  repository              = var.workspace.repository
+  repository_path         = var.workspace.repository_path
+  repository_branch       = var.workspace.repository_branch
+  provisioner_version     = var.workspace.provisioner_version
+  provider_connector      = var.workspace.provider_connector
+  repository_connector    = var.workspace.repository_connector
+  description             = try(var.workspace.description, null)
 
-  identifier              = each.value.identifier
-  name                    = each.value.name
-  org_id                  = each.value.org_id
-  project_id              = each.value.project_id
-  cost_estimation_enabled = each.value.cost_estimation_enabled
-  provisioner_type        = each.value.provisioner_type
-  repository              = each.value.repository
-  repository_path         = each.value.repository_path
-  repository_branch       = each.value.repository_branch
-  provisioner_version     = each.value.provisioner_version
-  provider_connector      = each.value.provider_connector
-  repository_connector    = each.value.repository_connector
-  description             = each.value.description
-  #tags                    = each.value.tags
-
-  # Terraform and Environment Variables
-  dynamic "terraform_variable" {
-    for_each = each.value.terraform_variables
-    content {
-      key        = terraform_variable.value.key
-      value      = terraform_variable.value.value
-      value_type = terraform_variable.value.value_type # Ensure this is defined in variables.tf
-    }
+  #################################
+  # Terraform Variables (Optional)
+  #################################
+  terraform_variable {
+    for_each = var.workspace.terraform_variables
+    key        = terraform_variable.value.key
+    value      = terraform_variable.value.value
+    value_type = terraform_variable.value.value_type
   }
 
-  # Define a block for environment_variable
-  dynamic "environment_variable" {
-    for_each = each.value.environment_variables
-    content {
-      key        = environment_variable.value.key
-      value      = environment_variable.value.value
-      value_type = environment_variable.value.value_type # Ensure this is defined in variables.tf
-    }
+  #################################
+  # Environment Variables (Optional)
+  #################################
+  environment_variable {
+    for_each = var.workspace.environment_variables
+    key        = environment_variable.value.key
+    value      = environment_variable.value.value
+    value_type = environment_variable.value.value_type
   }
 }
